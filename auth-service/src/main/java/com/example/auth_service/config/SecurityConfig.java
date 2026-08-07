6b9cc4ab72add201787d5dde056e5a59019e0e66
Ankit9767 <anktmb05@gmail.com>
2026-08-07 15:19:36 +0530

    commented out the other service code for now

--- auth-service/src/main/java/com/example/auth_service/config/SecurityConfig.java
+++ auth-service/src/main/java/com/example/auth_service/config/SecurityConfig.java
@@ -1,34 +1,34 @@
-package com.example.auth_service.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-
-@Configuration
-@EnableWebSecurity
-@RequiredArgsConstructor
-public class SecurityConfig {
-
-    private final DaoAuthenticationProvider authenticationProvider;
-
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http)
-            throws Exception {
-
-        http
-                .csrf(csrf -> csrf.disable())
-                .authenticationProvider(authenticationProvider)
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
-                        .requestMatchers("/actuator/**").permitAll()
-                        .anyRequest().authenticated()
-                );
-
-        return http.build();
-    }
-
-}
+//package com.example.auth_service.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+//
+//@Configuration
+//@EnableWebSecurity
+//@RequiredArgsConstructor
+//public class SecurityConfig {
+//
+//    private final DaoAuthenticationProvider authenticationProvider;
+//
+//    @Bean
+//    SecurityFilterChain securityFilterChain(HttpSecurity http)
+//            throws Exception {
+//
+//        http
+//                .csrf(csrf -> csrf.disable())
+//                .authenticationProvider(authenticationProvider)
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/auth/**").permitAll()
+//                        .requestMatchers("/actuator/**").permitAll()
+//                        .anyRequest().authenticated()
+//                );
+//
+//        return http.build();
+//    }
+//
+//}
