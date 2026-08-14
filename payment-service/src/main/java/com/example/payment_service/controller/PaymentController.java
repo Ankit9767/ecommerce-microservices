404bf300680cc7a233d65fd6abc88f8b3929152a
Ankit9767 <anktmb05@gmail.com>
2026-08-14 17:29:37 +0530

    commented code for resolving errors

--- payment-service/src/main/java/com/example/payment_service/controller/PaymentController.java
+++ payment-service/src/main/java/com/example/payment_service/controller/PaymentController.java
@@ -1,41 +1,41 @@
-package com.example.payment_service.controller;
-
-import com.ecommerce.common.dto.PaymentRequest;
-import com.ecommerce.common.dto.PaymentResponse;
-import com.example.payment_service.service.PaymentService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/payments")
-@RequiredArgsConstructor
-public class PaymentController {
-
-    private final PaymentService paymentService;
-
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public PaymentResponse processPayment(
-            @Valid @RequestBody PaymentRequest request) throws JsonProcessingException {
-
-        return paymentService.processPayment(request);
-    }
-
-    @GetMapping("/{id}")
-    public PaymentResponse getPaymentById(@PathVariable Long id) {
-
-        return paymentService.getPaymentById(id);
-    }
-
-    @GetMapping
-    public List<PaymentResponse> getAllPayments() {
-
-        return paymentService.getAllPayments();
-    }
-
-}
+//package com.example.payment_service.controller;
+//
+//import com.ecommerce.common.dto.PaymentRequest;
+//import com.ecommerce.common.dto.PaymentResponse;
+//import com.example.payment_service.service.PaymentService;
+//import com.fasterxml.jackson.core.JsonProcessingException;
+//import jakarta.validation.Valid;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("/payments")
+//@RequiredArgsConstructor
+//public class PaymentController {
+//
+//    private final PaymentService paymentService;
+//
+//    @PostMapping
+//    @ResponseStatus(HttpStatus.CREATED)
+//    public PaymentResponse processPayment(
+//            @Valid @RequestBody PaymentRequest request) throws JsonProcessingException {
+//
+//        return paymentService.processPayment(request);
+//    }
+//
+//    @GetMapping("/{id}")
+//    public PaymentResponse getPaymentById(@PathVariable Long id) {
+//
+//        return paymentService.getPaymentById(id);
+//    }
+//
+//    @GetMapping
+//    public List<PaymentResponse> getAllPayments() {
+//
+//        return paymentService.getAllPayments();
+//    }
+//
+//}
--- payment-service/src/main/java/com/example/payment_service/kafka/OrderCreatedConsumer.java
+++ payment-service/src/main/java/com/example/payment_service/kafka/OrderCreatedConsumer.java
@@ -1,48 +1,48 @@
-package com.example.payment_service.kafka;
-
-import com.ecommerce.common.events.OrderCreatedEvent;
-import com.ecommerce.common.kafka.KafkaTopics;
-import com.example.payment_service.service.PaymentService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Profile;
-import org.springframework.kafka.annotation.DltHandler;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.annotation.RetryableTopic;
-import org.springframework.retry.annotation.Backoff;
-import org.springframework.stereotype.Component;
-
-@Slf4j
-@Component
-@Profile("!test")
-@RequiredArgsConstructor
-public class OrderCreatedConsumer {
-
-    private final PaymentService paymentService;
-
-    @RetryableTopic(
-            attempts = "4",
-            backoff = @Backoff(delay = 3000),
-            dltTopicSuffix = "-dlt"
-    )
-    @KafkaListener(
-            topics = KafkaTopics.ORDER_CREATED,
-            groupId = "payment-group"
-    )
-    public void consume(OrderCreatedEvent event) throws JsonProcessingException {
-
-        log.info("Received Order Event : {}", event);
-
-        paymentService.processOrderEvent(event);
-
-    }
-
-    @DltHandler
-    public void handleDeadLetter(OrderCreatedEvent event) {
-
-        log.error("Received DLT Event {}", event);
-
-    }
-
-}
+//package com.example.payment_service.kafka;
+//
+//import com.ecommerce.common.events.OrderCreatedEvent;
+//import com.ecommerce.common.kafka.KafkaTopics;
+//import com.example.payment_service.service.PaymentService;
+//import com.fasterxml.jackson.core.JsonProcessingException;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.context.annotation.Profile;
+//import org.springframework.kafka.annotation.DltHandler;
+//import org.springframework.kafka.annotation.KafkaListener;
+//import org.springframework.kafka.annotation.RetryableTopic;
+//import org.springframework.retry.annotation.Backoff;
+//import org.springframework.stereotype.Component;
+//
+//@Slf4j
+//@Component
+//@Profile("!test")
+//@RequiredArgsConstructor
+//public class OrderCreatedConsumer {
+//
+//    private final PaymentService paymentService;
+//
+//    @RetryableTopic(
+//            attempts = "4",
+//            backoff = @Backoff(delay = 3000),
+//            dltTopicSuffix = "-dlt"
+//    )
+//    @KafkaListener(
+//            topics = KafkaTopics.ORDER_CREATED,
+//            groupId = "payment-group"
+//    )
+//    public void consume(OrderCreatedEvent event) throws JsonProcessingException {
+//
+//        log.info("Received Order Event : {}", event);
+//
+//        paymentService.processOrderEvent(event);
+//
+//    }
+//
+//    @DltHandler
+//    public void handleDeadLetter(OrderCreatedEvent event) {
+//
+//        log.error("Received DLT Event {}", event);
+//
+//    }
+//
+//}
