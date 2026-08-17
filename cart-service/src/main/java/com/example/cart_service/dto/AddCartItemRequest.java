b6d014fec67d0ef37f15288e9590047724e46a68
Ankit9767 <anktmb05@gmail.com>
2026-08-17 21:03:41 +0530

    add validations in dto

--- cart-service/src/main/java/com/example/cart_service/dto/AddCartItemRequest.java
+++ cart-service/src/main/java/com/example/cart_service/dto/AddCartItemRequest.java
@@ -2,10 +2,12 @@ package com.example.cart_service.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 public record AddCartItemRequest(
 
         @NotNull(message = "Product ID is required")
+        @Positive(message = "Product ID must be positive")
         Long productId,
 
         @NotNull(message = "Quantity is required")
