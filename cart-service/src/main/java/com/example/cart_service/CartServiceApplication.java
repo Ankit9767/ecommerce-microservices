6edd0fefb5ed403d445736607e65f64d359e09ad
Ankit9767 <anktmb05@gmail.com>
2026-08-17 15:47:54 +0530

    added pagination configuration for all the services in yml files

--- cart-service/src/main/java/com/example/cart_service/CartServiceApplication.java
+++ cart-service/src/main/java/com/example/cart_service/CartServiceApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "com.example.cart_service",
+        "com.ecommerce.common"
+})
 @EnableFeignClients
 public class CartServiceApplication {
 
