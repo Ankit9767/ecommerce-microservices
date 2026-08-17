ee6ae95dc44656ba60caa844942e0bb537dc8a6a
Ankit9767 <anktmb05@gmail.com>
2026-08-17 22:46:49 +0530

    added helm configuration for inventory service and also configured security config for it , created test profile for inventory-service

---
+++ inventory-service/src/main/java/com/example/inventory_service/InventoryServiceApplication.java
@@ -0,0 +1,20 @@
+package com.example.inventory_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.example.inventory_service",
+                "com.ecommerce.common"
+        }
+)
+@EnableFeignClients
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+}
