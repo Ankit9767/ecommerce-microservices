3ed0dc072f217927d95931e1d3af7a018148d3af
Ankit9767 <anktmb05@gmail.com>
2026-08-23 21:22:21 +0530

    updated init.sql to create new databases and users for it

--- helm/ecommerce-chart/init/init.sql
+++ helm/ecommerce-chart/init/init.sql
@@ -2,15 +2,21 @@ CREATE DATABASE IF NOT EXISTS product_db;
 CREATE DATABASE IF NOT EXISTS order_db;
 CREATE DATABASE IF NOT EXISTS payment_db;
 CREATE DATABASE IF NOT EXISTS auth_db;
+CREATE DATABASE IF NOT EXISTS cart_db;
+CREATE DATABASE IF NOT EXISTS inventory_db;
 
 CREATE USER IF NOT EXISTS 'product_user'@'%' IDENTIFIED BY 'product123';
 CREATE USER IF NOT EXISTS 'order_user'@'%' IDENTIFIED BY 'order123';
 CREATE USER IF NOT EXISTS 'payment_user'@'%' IDENTIFIED BY 'payment123';
 CREATE USER IF NOT EXISTS 'auth_user'@'%' IDENTIFIED BY 'auth123';
+CREATE USER IF NOT EXISTS 'cart_user'@'%' IDENTIFIED BY 'cart123';
+CREATE USER IF NOT EXISTS 'inventory_user'@'%' IDENTIFIED BY 'inventory123';
 
 GRANT ALL PRIVILEGES ON product_db.* TO 'product_user'@'%';
 GRANT ALL PRIVILEGES ON order_db.* TO 'order_user'@'%';
 GRANT ALL PRIVILEGES ON payment_db.* TO 'payment_user'@'%';
 GRANT ALL PRIVILEGES ON auth_db.* TO 'auth_user'@'%';
+GRANT ALL PRIVILEGES ON cart_db.* TO 'cart_user'@'%';
+GRANT ALL PRIVILEGES ON inventory_db.* TO 'inventory_user'@'%';
 
 FLUSH PRIVILEGES;
