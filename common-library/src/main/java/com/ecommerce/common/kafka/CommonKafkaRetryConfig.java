f7d845da7c47351d6bf3abefaf0fd7c53118792a
Ankit9767 <anktmb05@gmail.com>
2026-08-23 21:22:01 +0530

    handled error for kafka retry config

--- common-library/src/main/java/com/ecommerce/common/kafka/CommonKafkaRetryConfig.java
+++ common-library/src/main/java/com/ecommerce/common/kafka/CommonKafkaRetryConfig.java
@@ -1,13 +1,27 @@
 package com.ecommerce.common.kafka;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.EnableKafkaRetryTopic;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @Configuration
 @Profile("!test")
 @EnableKafka
 @EnableKafkaRetryTopic
+@EnableScheduling
 public class CommonKafkaRetryConfig {
+
+    @Bean
+    public TaskScheduler kafkaRetryTaskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(5);
+        scheduler.setThreadNamePrefix("kafka-retry-");
+        scheduler.initialize();
+        return scheduler;
+    }
 }
