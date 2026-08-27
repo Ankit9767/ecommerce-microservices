0f6f6b216a8f0d10c5b8de6b568a53264ba021b0
Ankit9767 <anktmb05@gmail.com>
2026-08-27 17:27:49 +0530

    handled error for scanning packages in common directory

--- product-service/src/main/java/com/example/product_service/ProductServiceApplication.java
+++ product-service/src/main/java/com/example/product_service/ProductServiceApplication.java
@@ -2,15 +2,24 @@ package com.example.product_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {
         "com.example.product_service",
         "com.ecommerce.common"
 })
+@EnableJpaRepositories(basePackages = {
+        "com.example.product_service",
+        "com.ecommerce.common.kafka"
+})
+@EntityScan(basePackages = {
+        "com.example.product_service",
+        "com.ecommerce.common.kafka"
+})
 public class ProductServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceApplication.class, args);
+    }
 }
