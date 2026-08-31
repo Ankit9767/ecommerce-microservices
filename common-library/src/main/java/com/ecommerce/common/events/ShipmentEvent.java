dfd0c990785b7875ce3aea09c931f2868ab45329
Ankit9767 <anktmb05@gmail.com>
2026-08-31 22:10:32 +0530

    added shipment events

---
+++ common-library/src/main/java/com/ecommerce/common/events/ShipmentEvent.java
@@ -0,0 +1,61 @@
+package com.ecommerce.common.events;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@SuperBuilder
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.EXISTING_PROPERTY,
+        property = "eventType",
+        visible = true
+)
+@JsonSubTypes({
+        @JsonSubTypes.Type(
+                value = ShipmentCreatedEvent.class,
+                name = "shipment-created"
+        ),
+        @JsonSubTypes.Type(
+                value = ShipmentShippedEvent.class,
+                name = "shipment-shipped"
+        ),
+        @JsonSubTypes.Type(
+                value = ShipmentInTransitEvent.class,
+                name = "shipment-in-transit"
+        ),
+        @JsonSubTypes.Type(
+                value = ShipmentOutForDeliveryEvent.class,
+                name = "shipment-out-for-delivery"
+        ),
+        @JsonSubTypes.Type(
+                value = ShipmentDeliveredEvent.class,
+                name = "shipment-delivered"
+        ),
+        @JsonSubTypes.Type(
+                value = ShipmentFailedEvent.class,
+                name = "shipment-failed"
+        ),
+        @JsonSubTypes.Type(
+                value = ShipmentCancelledEvent.class,
+                name = "shipment-cancelled"
+        )
+})
+public abstract class ShipmentEvent extends DomainEvent {
+
+    private Long shipmentId;
+
+    private Long orderId;
+
+    private Long customerId;
+
+    private String carrier;
+
+    private String trackingNumber;
+}
---
+++ common-library/src/main/java/com/ecommerce/common/events/ShipmentOutForDeliveryEvent.java
@@ -0,0 +1,13 @@
+package com.ecommerce.common.events;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@SuperBuilder
+public class ShipmentOutForDeliveryEvent extends ShipmentEvent {
+}
