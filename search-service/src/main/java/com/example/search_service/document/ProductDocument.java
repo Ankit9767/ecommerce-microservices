2e32ccf61b22a5c9b7bc51325cc190dabde42654
Ankit9767 <anktmb05@gmail.com>
2026-09-01 20:49:08 +0530

    added a product document entity for search service

---
+++ search-service/src/main/java/com/example/search_service/document/ProductDocument.java
@@ -0,0 +1,37 @@
+package com.example.search_service.document;
+
+import lombok.*;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document(indexName = "products")
+public class ProductDocument {
+
+    @Id
+    private Long productId;
+
+    @Field(type = FieldType.Text)
+    private String name;
+
+    @Field(type = FieldType.Keyword)
+    private String sku;
+
+    @Field(type = FieldType.Keyword)
+    private String category;
+
+    @Field(type = FieldType.Double)
+    private BigDecimal price;
+
+    @Field(type = FieldType.Boolean)
+    private Boolean active;
+}
