746b89733144a285efe18d09b2b5ae08872a3d49
Ankit9767 <anktmb05@gmail.com>
2026-09-01 22:57:55 +0530

    remove everything related to jpa because we are using elastic search in search service

--- search-service/src/main/java/com/example/search_service/SearchServiceApplication.java
+++ search-service/src/main/java/com/example/search_service/SearchServiceApplication.java
@@ -2,21 +2,8 @@ package com.example.search_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {
-        "com.example.search_service",
-        "com.ecommerce.common"
-})
-@EnableJpaRepositories(basePackages = {
-        "com.example.search_service",
-        "com.ecommerce.common.kafka"
-})
-@EntityScan(basePackages = {
-        "com.example.search_service",
-        "com.ecommerce.common.kafka"
-})
+@SpringBootApplication
 public class SearchServiceApplication {
 
     public static void main(String[] args) {
--- search-service/src/test/java/com/example/search_service/SearchServiceApplicationTests.java
+++ search-service/src/test/java/com/example/search_service/SearchServiceApplicationTests.java
@@ -1,12 +1,17 @@
 package com.example.search_service;
 
+import com.example.search_service.repository.ProductSearchRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class SearchServiceApplicationTests {
+
+    @MockBean
+    private ProductSearchRepository productSearchRepository;
 
 	@Test
 	void contextLoads() {
