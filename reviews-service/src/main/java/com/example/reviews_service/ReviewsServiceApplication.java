c7b35bf02d70236d9542c2774fcb19c82aff232c
Ankit9767 <anktmb05@gmail.com>
2026-09-07 00:31:47 +0530

    refactored ReviewsServiceApplication with correct paths for review service

--- reviews-service/src/main/java/com/example/reviews_service/ReviewsServiceApplication.java
+++ reviews-service/src/main/java/com/example/reviews_service/ReviewsServiceApplication.java
@@ -2,12 +2,27 @@ package com.example.reviews_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "com.example.reviews_service",
+        "com.ecommerce.common"
+})
+@EnableJpaRepositories(basePackages = {
+        "com.example.reviews_service",
+        "com.ecommerce.common.kafka"
+})
+@EntityScan(basePackages = {
+        "com.example.reviews_service",
+        "com.ecommerce.common.kafka"
+})
 public class ReviewsServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReviewsServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(
+                ReviewsServiceApplication.class,
+                args
+        );
+    }
 }
