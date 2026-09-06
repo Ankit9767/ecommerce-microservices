4b6c43838a8216d04dd278e141fd9285e8b27d33
Ankit9767 <anktmb05@gmail.com>
2026-09-06 16:51:16 +0530

    add security config for review service

---
+++ reviews-service/src/main/java/com/example/reviews_service/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.example.reviews_service.config;
+
+import com.ecommerce.common.security.GatewaySecurityConfiguration;
+import com.ecommerce.common.security.RoleSecurity;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+
+@Configuration
+@EnableMethodSecurity
+@Import({GatewaySecurityConfiguration.class, RoleSecurity.class})
+public class SecurityConfig {
+
+}
