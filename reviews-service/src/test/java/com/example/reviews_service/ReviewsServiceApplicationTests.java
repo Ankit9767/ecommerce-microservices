79e4e8b47702e6060ab7e2909b2f5b89086c86f9
Ankit9767 <anktmb05@gmail.com>
2026-09-06 16:22:03 +0530

    added test environment for review service

---
+++ reviews-service/src/test/java/com/example/reviews_service/ReviewsServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.reviews_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ReviewsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
