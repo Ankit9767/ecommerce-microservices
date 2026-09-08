7730eec4ebe5d83dcfcd008ec94ad2713cc3f478
Ankit9767 <anktmb05@gmail.com>
2026-09-08 18:47:46 +0530

    enable Feign clients and scheduling for review service

--- reviews-service/src/main/java/com/example/reviews_service/ReviewsServiceApplication.java
+++ reviews-service/src/main/java/com/example/reviews_service/ReviewsServiceApplication.java
@@ -3,7 +3,9 @@ package com.example.reviews_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = {
         "com.example.reviews_service",
@@ -17,6 +19,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
         "com.example.reviews_service",
         "com.ecommerce.common.kafka"
 })
+@EnableFeignClients
+@EnableScheduling
 public class ReviewsServiceApplication {
 
     public static void main(String[] args) {
