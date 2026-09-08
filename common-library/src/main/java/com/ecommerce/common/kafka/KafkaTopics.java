2627e998607ff61c42bd246c29d27ebd255165b0
Ankit9767 <anktmb05@gmail.com>
2026-09-08 18:47:22 +0530

    add review-created event constant for review service

--- common-library/src/main/java/com/ecommerce/common/kafka/KafkaTopics.java
+++ common-library/src/main/java/com/ecommerce/common/kafka/KafkaTopics.java
@@ -21,6 +21,8 @@ public final class KafkaTopics {
 
     public static final String NOTIFICATION_SENT = "notification-sent";
 
+    public static final String REVIEW_CREATED = "review-created";
+
     /*
      * Kafka message header that carries the fine-grained event type for
      * observability.
