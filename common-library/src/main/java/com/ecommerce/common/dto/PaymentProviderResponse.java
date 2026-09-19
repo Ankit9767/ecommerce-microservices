df3b080e342090c7c8ac612c98d4c8495a1e9aa8
Ankit9767 <anktmb05@gmail.com>
2026-09-19 17:37:06 +0530

    add provider order and payment IDs to response

--- common-library/src/main/java/com/ecommerce/common/dto/PaymentProviderResponse.java
+++ common-library/src/main/java/com/ecommerce/common/dto/PaymentProviderResponse.java
@@ -3,6 +3,8 @@ package com.ecommerce.common.dto;
 import com.ecommerce.common.enums.PaymentStatus;
 
 public record PaymentProviderResponse(
+        String providerOrderId,
+        String providerPaymentId,
         String providerReference,
         PaymentStatus status,
         String failureReason
