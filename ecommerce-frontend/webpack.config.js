a877e7a3a71c4d811435669866519a974daffa0e
Ankit9767 <anktmb05@gmail.com>
2026-09-23 19:31:56 +0530

    added frontend files for the project

---
+++ ecommerce-frontend/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.jsx",
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "assets/[name].[contenthash].js",
+    clean: true,
+    publicPath: "/"
+  },
+
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    })
+  ],
+
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+    hot: true,
+    open: true
+  }
+};
