9d7d79ae40ca11a4c6025b94b711410483131a7a
Ankit9767 <anktmb05@gmail.com>
2026-09-24 16:18:25 +0530

    wrap app with cart provider for global cart state

--- ecommerce-frontend/src/index.jsx
+++ ecommerce-frontend/src/index.jsx
@@ -3,16 +3,18 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
+import { CartProvider } from "./context/CartContext";
 import "./styles/global.css";
 
 const rootElement = document.getElementById("root");
-
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <CartProvider>
+        <App />
+      </CartProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
