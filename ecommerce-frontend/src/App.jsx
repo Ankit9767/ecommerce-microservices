f3428a85336070bb779f8978f3b363a523c47d3f
Ankit9767 <anktmb05@gmail.com>
2026-09-26 17:19:08 +0530

    add checkout and order routes

--- ecommerce-frontend/src/App.jsx
+++ ecommerce-frontend/src/App.jsx
@@ -12,28 +12,39 @@ import Cart from "./pages/Cart";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import NotFound from "./pages/NotFound";
+import Checkout from "./pages/Checkout";
+import Orders from "./pages/Orders";
+import OrderDetails from "./pages/OrderDetails";
 
 function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
+
         <Route path="/" element={<Home />} />
 
         <Route path="/products" element={<Products />} />
+
         <Route path="/products/:id" element={<ProductDetails />} />
 
         <Route path="/categories" element={<Categories />} />
-        <Route
-          path="/categories/:id"
-          element={<CategoryProducts />}
-        />
+
+        <Route path="/categories/:id" element={<CategoryProducts />} />
+
+        <Route path="/checkout" element={<Checkout />} />
+
+        <Route path="/orders" element={<Orders />} />
+
+        <Route path="/orders/:id" element={<OrderDetails />} />
 
         <Route path="/cart" element={<Cart />} />
 
         <Route path="/login" element={<Login />} />
+
         <Route path="/register" element={<Register />} />
 
         <Route path="*" element={<NotFound />} />
+        
       </Route>
     </Routes>
   );
